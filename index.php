f2ba6013bdf751a53f6c0743e8c57910469996b5
aldo170 <jovaldogosall@gmail.com>
2026-09-23 22:41:12 +0700

    Add quiz index page

---
+++ index.php
@@ -0,0 +1,227 @@
+<?php
+session_start();
+
+require_once("class/soal.php");
+require_once("class/jawaban.php");
+
+$objSoal = new soal();
+$objJawaban = new jawaban();
+
+$halamanPertama = $objSoal->getHalamanPertama();
+
+$halaman =
+isset($_GET['halaman']) ?
+$_GET['halaman'] :
+$halamanPertama;
+
+if(!is_numeric($halaman)){
+    $halaman = $halamanPertama;
+}
+
+if(isset($_POST['aksi'])){
+
+    $halaman = $_POST['halaman'];
+
+    if(!is_numeric($halaman)){
+        $halaman = $halamanPertama;
+    }
+
+    if(isset($_POST['jawaban'])){
+
+        foreach($_POST['jawaban'] as $idsoal => $idjawaban){
+
+            $_SESSION['jawaban'][$idsoal] = $idjawaban;
+
+            $benarkah =
+            $objJawaban->cekJawaban($idjawaban);
+
+            $_SESSION['status'][$idsoal] = $benarkah;
+        }
+    }
+
+    if($_POST['aksi'] == "Previous"){
+
+        $tujuan =
+        $objSoal->getHalamanSebelumnya($halaman);
+
+        if(!is_null($tujuan)){
+
+            header(
+                "location:index.php?halaman=".
+                $tujuan
+            );
+        }
+    }
+
+    else if($_POST['aksi'] == "Next"){
+
+        $tujuan =
+        $objSoal->getHalamanBerikutnya($halaman);
+
+        if(is_null($tujuan)){
+
+            header("location:kesimpulan.php");
+
+        }else{
+
+            header(
+                "location:index.php?halaman=".
+                $tujuan
+            );
+        }
+    }
+}
+
+$resSoal =
+$objSoal->getSoalByHalaman($halaman);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0">
+
+    <title>Quiz Fullstack</title>
+
+    <link
+    rel="stylesheet"
+    href="css/style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+
+<h1>Quiz Fullstack</h1>
+
+<p>
+Halaman:
+<?php echo $halaman; ?>
+</p>
+
+<form
+action="index.php?halaman=<?php echo $halaman; ?>"
+method="post">
+
+<?php
+
+while($rowSoal = $resSoal->fetch_assoc()){
+
+?>
+
+<div class="soal">
+
+<h3>
+
+<?php
+
+echo
+$rowSoal["nomor"].
+". ".
+$rowSoal["pertanyaan"];
+
+?>
+
+</h3>
+
+<?php
+
+$resJawaban =
+$objJawaban->getJawabanBySoal(
+    $rowSoal["idsoal"]
+);
+
+while(
+    $rowJawaban =
+    $resJawaban->fetch_assoc()
+){
+
+    $checked = "";
+
+    if(
+        isset(
+            $_SESSION['jawaban']
+            [$rowSoal["idsoal"]]
+        )
+    ){
+
+        if(
+            $_SESSION['jawaban']
+            [$rowSoal["idsoal"]]
+            ==
+            $rowJawaban["idjawaban"]
+        ){
+
+            $checked = "checked";
+        }
+    }
+
+?>
+
+<label class="jawaban">
+
+<input
+type="radio"
+name="jawaban[<?php echo $rowSoal["idsoal"]; ?>]"
+value="<?php echo $rowJawaban["idjawaban"]; ?>"
+<?php echo $checked; ?>
+>
+
+<?php
+echo $rowJawaban["isi_jawaban"];
+?>
+
+</label>
+
+<?php
+}
+?>
+
+</div>
+
+<?php
+}
+?>
+
+<input
+type="hidden"
+name="halaman"
+value="<?php echo $halaman; ?>">
+
+<?php
+
+$halamanSebelumnya =
+$objSoal->getHalamanSebelumnya($halaman);
+
+if(!is_null($halamanSebelumnya)){
+
+?>
+
+<input
+type="submit"
+name="aksi"
+value="Previous">
+
+<?php
+}
+?>
+
+<input
+type="submit"
+name="aksi"
+value="Next">
+
+</form>
+
+</div>
+
+</body>
+
+</html>
